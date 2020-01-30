936607a7d4e8e1e9714c922a7f124695f3b1903e
--replace-all <emh221@gmail.com>
2020-01-30 15:55:30 -0800

    make it possible to render price and ingredient, stored in firebase to an orders page in the app.

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -5,18 +5,23 @@ import ContactData from './ContactData/ContactData'
 
 class Checkout extends Component {
   state = {
-    ingredients: null
+    ingredients: null,
+    price: 0
     
   }
 
   componentWillMount() {
     const query = new URLSearchParams(this.props.location.search);
     const ingredients = {};
+    let price = 0;
     for (let param of query.entries()) {
-    
+        if(param[0] === 'price'){
+            price = param[1];
+        } else {
             ingredients[param[0]] = +param[1];
+        }
     }
-    this.setState({ ingredients: ingredients});
+    this.setState({ ingredients: ingredients, totalPrice : price});
   }
 
   checkoutCancelledHandler = () => {
@@ -37,7 +42,7 @@ class Checkout extends Component {
         />
         <Route
           path={this.props.match.path + "/contact-data"}
-          render = {() => (<ContactData ingredients = {this.state.ingredients} price ={this.state.totalPrice}/>)}
+          render = {(props) => (<ContactData ingredients = {this.state.ingredients} price ={this.state.totalPrice} {...props}/>)}
         />
       </div>
     );
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Button from "../../../components/UI/Button/Button";
+import Spinner from '../../../components/UI/Spinner/Spinner'
 import Classes from "./ContactData.module.css";
 import axios from "../../../axios-orders";
 
@@ -35,46 +36,51 @@ class ContactData extends Component {
     axios
       .post("/orders.json", order)
       .then(response => {
-        this.setState({ loading: false, isBuying: false });
+        this.setState({ loading: false});
+        this.props.history.push('/');
       })
       .catch(err => {
-        this.setState({ loading: false, isBuying: false });
+        this.setState({ loading: false});
       });
   };
 
   render() {
+      let form = (<form>
+        <input
+          className={Classes.Input}
+          type="text"
+          name="name"
+          placeholder="Your Name"
+        />
+        <input
+          className={Classes.Input}
+          type="text"
+          name="email"
+          placeholder="Your Email"
+        />
+        <input
+          className={Classes.Input}
+          type="text"
+          name="street"
+          placeholder="Your Street"
+        />
+        <input
+          className={Classes.Input}
+          type="text"
+          name="postalCode"
+          placeholder="Your Zip Code"
+        />
+        <Button btnType="Success" clicked={this.orderHandler}>
+          ORDER
+        </Button>
+      </form>);
+      if(this.state.loading){
+          form = <Spinner/>
+      }
     return (
       <div className={Classes.ContactData}>
         <h4>Enter Your Contact Data</h4>
-        <form>
-          <input
-            className={Classes.Input}
-            type="text"
-            name="name"
-            placeholder="Your Name"
-          />
-          <input
-            className={Classes.Input}
-            type="text"
-            name="email"
-            placeholder="Your Email"
-          />
-          <input
-            className={Classes.Input}
-            type="text"
-            name="street"
-            placeholder="Your Street"
-          />
-          <input
-            className={Classes.Input}
-            type="text"
-            name="postalCode"
-            placeholder="Your Zip Code"
-          />
-          <Button btnType="Success" clicked={this.orderHandler}>
-            ORDER
-          </Button>
-        </form>
+        {form}
       </div>
     );
   }
